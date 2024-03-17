5035d7943fc606fb58f45e1fc13163beb95aad7b
dleovl <95427722+dleovl@users.noreply.github.com>
2024-03-17 02:32:47 -0400

    Deployment 3

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-function calculate() {
-    let xpResult = getLevel(document.getElementById("targetLevel").value) - getLevel(document.getElementById("currentLevel").value);
-    let maxMessages = Math.ceil(xpResult / 11);
-    let avgMessages = Math.ceil(xpResult / 5.5);
-    document.getElementById("xpResult").innerHTML = xpResult.toString();
-    document.getElementById("maxMessages").innerHTML = maxMessages.toString();
-    document.getElementById("avgMessages").innerHTML = avgMessages.toString();
-}
-
-function getLevel(currentXp = 0) {
+function XPFromLevel(currentLevel = 0) {
     let level = 0;
     let xp = 0;
-    while (level < (currentXp - 1)) {
-        xp = xp + 45 * level * (Math.floor(level / 10)+1);
+    while (level < (currentLevel - 1)) {
+        xp = xp + 45 * level * (Math.floor(level / 10) + 1);
         level++;
     }
     return xp;
+}
+
+function levelFromXP(currentXP = 0) {
+    let level = 0;
+    let xp = 0;
+
+    while (true) {
+        const nextXp = xp + 45 * level * (Math.floor(level / 10) + 1);
+        if (nextXp > currentXP) {
+            return level + 1;
+        }
+        xp = nextXp;
+        level++;
+    }
+}
+
+function calculate() {
+    let currentXP = document.getElementById("currentXP").value;
+    let xpResult = Math.max((XPFromLevel(document.getElementById("targetLevel").value) - currentXP), 0);
+    let minMessages = Math.ceil(xpResult / 11);
+    let avgMessages = Math.ceil(xpResult / 5.5);
+    document.getElementById("xpResult").innerHTML = xpResult.toString();
+    document.getElementById("minMessages").innerHTML = minMessages.toString();
+    document.getElementById("avgMessages").innerHTML = avgMessages.toString();
 }
 
 let inputs = document.querySelectorAll(".inputBlock input")
