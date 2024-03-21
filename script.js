ba7a4a4113ce4dbf2189a9a620bdb0e52755cf4a
dleovl <95427722+dleovl@users.noreply.github.com>
2024-03-21 19:39:49 -0400

    Revamp custom XP
    Additionally, make stretch mode current XP a requirement

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+function calculate() {
+    let currentXP = parseInt($("#currentXP").val()) || 0;
+    let targetLevelXP = levelToXP(parseInt($("#targetLevel").val())) || 0;
+    if (targetLevelXP !== 0 && currentXP !== 0) {
+        let xpResult = Math.max(targetLevelXP - currentXP, 0);
+        let minMessages = Math.ceil(xpResult / 11);
+        $("#xpResult").text(`${xpResult} more`);
+        $("#levelResult").text($("#targetLevel").val());
+        $("#levelTotal").text(targetLevelXP);
+        $("#minMessages").text(minMessages);
+        $("#avgMessages").text(minMessages * 2);
+    } else {
+        $("#xpResult").text("< 45");
+        $("#levelResult").text($("#targetLevel").val() || "0");
+        $("#levelTotal").text("< 45");
+        $("#minMessages").text(0);
+        $("#avgMessages").text(0);
+    }
+}
+
+function customXPToLevel() {
+    let xp = $("#customXP").val() || 0;
+    if (xp < 45) {
+        $("#customResult").text("XP below 45 cannot be converted to level");
+    } else {
+        let level = xpToLevel(xp);
+        let currentLevelBaseXP = levelToXP(level);
+        let nextLevelBaseXP = levelToXP(level + 1);
+        $("#customResult").html(`${xp} XP translates to level ${xpToLevel(xp)}<br>${nextLevelBaseXP - xp} more XP needed for level ${level + 1}<br>From level ${level} to ${level + 1}, you're ${(((xp - currentLevelBaseXP) / (nextLevelBaseXP - currentLevelBaseXP)) * 100).toFixed(2)}% there`);
+    }
+}
+
+function customLevelToXP() {
+    let level = $("#customLevel").val() || 0;
+    if (level < 3) {
+        $("#customResult").text("Level below 3 cannot be converted to XP");
+    } else {
+        let xp = levelToXP(level);
+        let minMessages = Math.ceil(xp / 11);
+        $("#customResult").html(`Level ${level} requires ${xp} XP total<br>${minMessages} messages minimum (11 XP/msg)<br>${minMessages * 2} messages average (5.5 XP/msg)`);    
+    }
+}
+
 function levelToXP(currentLevel = 0) {
     let level = 0;
     let xp = 0;
@@ -41,52 +84,17 @@ $(document).ready(function() {
     });
 });
 
-function calculate() {
-    let currentXP = parseInt($("#currentXP").val()) || 0;
-    let targetLevelXP = levelToXP(parseInt($("#targetLevel").val())) || 0;
-    if (targetLevelXP !== 0) {
-        let xpResult = Math.max(targetLevelXP - currentXP, 0);
-        let minMessages = Math.ceil(xpResult / 11);
-        $("#xpResult").text(`${xpResult} more`);
-        $("#levelResult").text($("#targetLevel").val());
-        $("#levelTotal").text(targetLevelXP);
-        $("#minMessages").text(minMessages);
-        $("#avgMessages").text(minMessages * 2);
-    } else {
-        $("#xpResult").text("Less than 45");
-        $("#levelResult").text($("#targetLevel").val() || "0");
-        $("#levelTotal").text("<45");
-        $("#minMessages").text(0);
-        $("#avgMessages").text(0);
-    }
-}
-
-function customXPToLevel() {
-    let xp = $("#customXP").val() || 0;
-    if (xp < 45) {
-        $("#customResult").text("XP below 45 cannot be converted to level");
-    } else {
-        $("#customResult").text(`${xp} XP is level ${xpToLevel(xp)}`);
-    }
-}
-
-function customLevelToXP() {
-    let level = $("#customLevel").val() || 0;
-    $("#customResult").text(`Level ${level} is ${levelToXP(level)} XP`);
-}
-
 var isMainBlock = true;
 function switchModes() {
     isMainBlock = !isMainBlock;
     $("#xpResult, #levelResult, #levelTotal, #minMessages, #avgMessages").text(0);
     $("#currentXP, #targetLevel, #customXP, #customLevel").val("");
     $("#customResult").text("No results");
-    $("#levelUpBlock, #levelUpButtons").toggleClass("hidden");
-    $("#customBlock, #customButtons").toggleClass("hidden");
+    $("#levelUpBlock, #levelUpButtons, #customBlock, #customButtons").toggleClass("hidden");
     if (isMainBlock) {
-        $("#modeButton").text("Custom Mode");
+        $("#page sup").text("Stretch Mode");
     } else {
-        $("#modeButton").text("Normal Mode");
+        $("#page sup").text("Custom Mode");
     }
 }
 
